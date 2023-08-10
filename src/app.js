4353a588147bca298e0f37d1317da1a664c9ac16
Cristian Mozetich <cristianmozetich@gmail.com>
2023-08-10 21:06:24 +0100

    tercer desafio

--- src/app.js
+++ src/app.js
@@ -10,20 +10,34 @@ const PORT = 4000
 
 
 app.get('/products', async (req, res) => {
-    const products = await productManager.getProducts()
+    try{
+        const products = await productManager.getProducts()
 
-    const { category } = req.query
+        const { limit } = req.query
+    
+        const productos = products.slice( 0, limit )
+    
+        res.send(productos)
 
-    const productos = products.filter( prod => prod.category === category)
+    } catch{
+        console.error(error)
+    }
 
-    res.send(productos)
+
 });
 
 app.get('/products/:id', async(req, res)=>{
-    const products = await productManager.getProducts()
 
-    const prod = products.find( prod => prod.id === parseInt(req.params.id))
-    res.send(prod)
+    try{
+        const products = await productManager.getProducts()
+
+        const prod = products.find( prod => prod.id === parseInt(req.params.id))
+        res.send(prod)
+
+    } catch{
+        console.error(error)
+    }
+
 })
 
 app.listen(PORT, ()=>{
