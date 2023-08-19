ac40b404408f7465193a7e8cd52c415cf2e99c17
Cristian Mozetich <cristianmozetich@gmail.com>
2023-08-20 00:04:06 +0100

    trabajando metodo join en ruta static

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import prodsRouter from "./routes/products.routes.js";
 import cartRouter from "./routes/carts.routes.js";
+import { __dirname } from "./path.js";
+import path from "path";
 
 const app = express()
 
@@ -15,6 +17,10 @@ app.use(express.urlencoded({ extended: true }))
 //Routes
 app.use('/api/products', prodsRouter)
 app.use('/api/carts', cartRouter)
+app.use('/static', express.static(path.join(__dirname, '/public')));
+
+
+
 
 app.listen(PORT, ()=>{
     console.log( `server on port ${PORT} `)
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import cartManager from '../cartManager.js';
-import { productManager } from '../productManager.js';
 import { error } from 'console';
 
 
