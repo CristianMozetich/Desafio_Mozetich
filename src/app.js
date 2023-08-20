3f15f76b2fde9881d3709a8dd73fc7a55768b10d
Cristian Mozetich <cristianmozetich@gmail.com>
2023-08-20 10:56:04 +0100

    caga de imagenes desde postman

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import prodsRouter from "./routes/products.routes.js";
 import cartRouter from "./routes/carts.routes.js";
 import { __dirname } from "./path.js";
 import path from "path";
+import multer from "multer";
 
 const app = express()
 
@@ -14,10 +15,28 @@ app.use(express.urlencoded({ extended: true }))
 
 
 
+
+//Config Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'src/public/img')
+    },
+    filename: ( req, file, cb )=>{
+        cb(null, `${Date.now()}${file.originalname}`) //concateno el nombre original de mi archivo con milisegundos Date.now()
+    }
+})
+const upload = multer({storage: storage})
+
 //Routes
 app.use('/api/products', prodsRouter)
 app.use('/api/carts', cartRouter)
 app.use('/static', express.static(path.join(__dirname, '/public')));
+
+app.post('/upload', upload.single('product'), (req, res)=>{
+console.log(req.file)
+console.log(req.body)
+    res.status(200).send("imagen cargada")
+})
 
 
 
